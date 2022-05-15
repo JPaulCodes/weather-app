67d77675f76f9872c475325877315d7741028ff3
JonPaulCode <jwatson88@live.co.uk>
2022-05-15 15:43:42 +0100

    Add utility to generate elements from HTML

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -25,4 +25,11 @@ const weatherApi = (() => {
   };
 })();
 
-export { weatherApi };
+function createElementFromHtml(htmlString) {
+  const template = document.createElement('template');
+  template.innerHTML = htmlString.trim();
+
+  return template.content.firstElementChild;
+}
+
+export { weatherApi, createElementFromHtml };
