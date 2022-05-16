1c6e29ee2fddc6527e0d1576da4d9cb46ef1a9a2
JonPaulCode <jwatson88@live.co.uk>
2022-05-16 18:38:43 +0100

    Add wind speed descriptions

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -32,4 +32,42 @@ function createElementFromHtml(htmlString) {
   return template.content.firstElementChild;
 }
 
-export { weatherApi, createElementFromHtml };
+function getWindDescription(windSpeed, units) {
+  let windDescription = '';
+  let ms = 0;
+
+  if (units === 'imperial') ms = windSpeed / 2.2369;
+  else ms = windSpeed;
+
+  if (ms < 0.5) {
+    windDescription = 'Calm';
+  } else if (ms < 1.6) {
+    windDescription = 'Light Air';
+  } else if (ms < 3.4) {
+    windDescription = 'Light Breeze';
+  } else if (ms < 5.6) {
+    windDescription = 'Gentle Breeze';
+  } else if (ms < 8) {
+    windDescription = 'Moderate Breeze';
+  } else if (ms < 10.8) {
+    windDescription = 'Fresh Breeze';
+  } else if (ms < 13.9) {
+    windDescription = 'Strong Breeze';
+  } else if (ms < 17.2) {
+    windDescription = 'High Wind';
+  } else if (ms < 20.8) {
+    windDescription = 'Gale';
+  } else if (ms < 24.5) {
+    windDescription = 'Strong Gale';
+  } else if (ms < 28.5) {
+    windDescription = 'Storm';
+  } else if (ms < 32.7) {
+    windDescription = 'Violent Storm';
+  } else if (ms >= 32.7) {
+    windDescription = 'Hurricane';
+  }
+
+  return windDescription;
+}
+
+export { weatherApi, createElementFromHtml, getWindDescription };
