eec644a140f025a91573fd3dcc4e8c120feff91f
JonPaulCode <jwatson88@live.co.uk>
2022-05-16 18:35:57 +0100

    Add functionality to left weather display

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
-import { weatherApi } from './utilities';
+import { weatherApi, getWindDescription } from './utilities';
 
 const weatherForecast = (() => {
   let currentCity = 'london';
@@ -59,6 +59,37 @@ const cityTimeDisplay = {
   },
 };
 
+const leftWeatherDisplay = {
+  async init() {
+    this.data = await weatherForecast.getData();
+    this.units = weatherForecast.getUnits();
+    this.cacheDom();
+    this.renderWeatherData();
+  },
+
+  cacheDom() {
+    this.temperatureNow = document.querySelector('.temperature-now');
+    this.weatherNow = document.querySelector('.weather-now');
+    this.feelsLike = document.querySelector('.feels-like');
+    this.windNow = document.querySelector('.wind-now');
+  },
+
+  renderWeatherData() {
+    const { weatherData } = this.data;
+    const tempUnits = this.units === 'metric' ? '°C' : '°F';
+    const windSpeed = weatherData.current.wind_speed;
+    const windDescription = getWindDescription(windSpeed, this.units);
+
+    this.temperatureNow.innerText = `${Math.round(weatherData.current.temp)}${tempUnits}`;
+    this.weatherNow.innerText = weatherData.current.weather[0].description;
+    this.feelsLike.innerText = `Feels Like ${Math.round(
+      weatherData.current.feels_like,
+    )}${tempUnits}`;
+    this.windNow.innerText = windDescription;
+  },
+};
+
 (() => {
   cityTimeDisplay.init();
+  leftWeatherDisplay.init();
 })();
